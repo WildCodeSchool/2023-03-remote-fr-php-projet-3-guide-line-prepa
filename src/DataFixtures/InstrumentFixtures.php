0d29b70b314470f05fafa7d3a7f180b22c5e2639
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-06-07 20:15:40 +0200

    Instrument entity + fixtures with pictures

---
+++ src/DataFixtures/InstrumentFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Instrument;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class InstrumentFixtures extends Fixture
+{
+    public const INSTRUMENTS = [
+        ['name' => 'Stratocaster 1958',
+            'picture' => 'stratocaster-gilmour-black.jpg'],
+        ['name' => 'Les Paul 1975',
+            'picture' => 'gibson-les-paul.jpeg',],
+        ['name' => 'Télécaster 1969',
+            'picture' => 'telecaster.jpeg',],
+        ['name' => 'Martin NY',
+            'picture' => 'martin-ny.jpeg',],
+        ['name' => 'Stratocaster série L',
+            'picture' => 'stratocaster-gilmour-white.jpg',],
+        ['name' => 'Jaguar',
+            'picture' => 'fender-curt-kobain-jaguar.jpeg',],
+        ['name' => 'SG', 'company' => 'Gibson',
+            'picture' => 'gibson-sg.jpeg',],
+        ['name' => 'Martin D-35',
+            'picture' => 'gilmour-martin-d-35.jpg',],
+        ['name' => '335',
+            'picture' => 'gibson-335.jpeg',],
+        ['name' => 'Gretsch White Penguin 6134',
+            'picture' => 'fred-gretsch-white-penguin-6134-gilmour.jpg',],
+        ['name' => 'Martin D-45',
+            'picture' => '05-GRUHN-D-45.jpeg',],
+    ];
+
+    public function __construct(private ParameterBagInterface $parameterBag)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $uploadInstrumentDir = $this->parameterBag->get('upload_instrument_dir');
+        if (!is_dir(__DIR__ . '/../../public/' . $uploadInstrumentDir)) {
+            //créer un dossier de manière récursive
+            mkdir(__DIR__ . '/../../public/' . $uploadInstrumentDir, recursive: true);
+        }
+        foreach (self::INSTRUMENTS as $instrumentData) {
+            copy(
+                __DIR__ . '/data/instruments/' . $instrumentData['picture'],
+                __DIR__ . '/../../public/' . $uploadInstrumentDir . '/' . $instrumentData['picture']
+            );
+            $instrument = new Instrument();
+            $instrument
+                ->setName($instrumentData['name'])
+                ->setPicture($instrumentData['picture']);
+            $manager->persist($instrument);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Instrument.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstrumentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstrumentRepository::class)]
+class Instrument
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
