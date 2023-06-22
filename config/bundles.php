f8af60191527ca7488391301555d1bddf3ec6423
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-06-22 18:43:04 +0200

    install turbo

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
